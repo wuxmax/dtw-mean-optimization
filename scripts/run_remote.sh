7a65a06c7d847c635c19a2db28a37e0981b4f6cb
wuxmax <max.reinhard@campus.tu-berlin.de>
2020-06-18 14:52:12 +0200

    fucking hell

--- scripts/run_remote.sh
+++ scripts/run_remote.sh
@@ -6,5 +6,5 @@ if [[ "$ALWAYS_BUILD" == 1 ]]; then
     scripts/build_image.sh
 fi
 
-# docker run -d --name=$USER-$(uuidgen) --user $(id -u):$(id -u) -it --gpus all -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME remote
-docker run -d --name=$USER-$(uuidgen) -it --gpus all -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME remote
+docker run -d --name=$USER-$(uuidgen) --user $(id -u):$(id -u) -it --gpus all -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME remote
+# docker run -d --name=$USER-$(uuidgen) -it --gpus all -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME remote
