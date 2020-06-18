61fd0c681a1b7b46892c65c1eccb7b1c91bc6b3f
wuxmax <max.reinhard@campus.tu-berlin.de>
2020-06-18 02:28:39 +0200

    minor fix

--- scripts/run_experiment_remote.sh
+++ scripts/run_experiment_remote.sh
@@ -1,3 +1,3 @@
 source .env
 
-docker run --name=$USER-$(uuidgen) --user $(id -u):$(id -g) -it --gpus all -v $(pwd)/results:/results $IMAGE_NAME remote
+docker run --name=$USER-$(uuidgen) --user $(id -u):$(id -g) -it --gpus all -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME remote
