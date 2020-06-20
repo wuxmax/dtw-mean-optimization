2b5e3185457ee51edbe1470a5cdb9d909e3bd903
wuxmax <max.reinhard@campus.tu-berlin.de>
2020-06-20 19:53:49 +0200

    minor fix

--- scripts/prepare_vultr.sh
+++ scripts/prepare_vultr.sh
@@ -1,6 +1,4 @@
-apt install pyhton3-pip
-pyhton3 -m venv venv
-source venv/bin/activate
+apt install -y python3-pip unzip
 
 mkdir results
 mkdir datasets
@@ -8,3 +6,5 @@ mkdir datasets
 wget https://www.cs.ucr.edu/~eamonn/time_series_data_2018/UCRArchive_2018.zip
 unzip -P someone UCRArchive_2018.zip
 
+pyhton3 -m venv venv
+source venv/bin/activate
