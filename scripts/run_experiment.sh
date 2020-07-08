424c1403d5273c8a809d2650dfdcdbfbe59bfe17
wuxmax <max.reinhard@campus.tu-berlin.de>
2020-07-08 14:43:53 +0200

    updated docker command

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -11,4 +11,4 @@ if [[ -n "$1" ]]; then
     CONFIG="$1"
 fi
 
-docker run -d -it --rm --name=$USER-$(uuidgen) -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME $CONFIG -d /datasets/UCRArchive_2018 -r /results
+docker run -dt --name=$USER-$(uuidgen) -v $(pwd)/datasets:/datasets -v $(pwd)/results:/results $IMAGE_NAME $CONFIG -d /datasets/UCRArchive_2018 -r /results
