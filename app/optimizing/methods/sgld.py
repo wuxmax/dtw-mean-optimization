d383850a22d218e06823e5809e43380a0c4f0c1b
Faraz M <f.maschhur@campus.tu-berlin.de>
2020-07-22 15:19:40 +0200

    minor fix

--- app/optimizing/methods/sgld.py
+++ app/optimizing/methods/sgld.py
@@ -7,17 +7,22 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+'''
+This file implements the SSGLD algorithm - an subgradient and dtw able version
+of the SGLD algorithm introduced in the following paper:
+https://www.researchgate.net/publication/313857927_A_Hitting_Time_Analysis_of_Stochastic_Gradient_Langevin_Dynamics
+'''
 def run(X, z, f, batch_size, n_coverage, n_epochs, d_converged, rng):
 
-    X = np.array(X)
+    X = np.array(X) # sample input
     N = X.shape[0]  # amount of samples
     d = X.shape[1]  # dimension of each sample
-    m = X.shape[2]
+    m = X.shape[2]  # dimension of each datapoint of each sample
 
     n_steps = int(np.ceil(n_coverage/batch_size))  # iterations to go through
 
-    n_visited_samples = 0
-    t = 0
+    n_visited_samples = 0 # count how many samples we visited
+    t = 0 # iteration step index
 
     kmin = np.amin(X, axis=0)  # lower limit of solution
     kmax = np.amax(X, axis=0)  # higher limit of solution
@@ -37,7 +42,7 @@ def run(X, z, f, batch_size, n_coverage, n_epochs, d_converged, rng):
 
         for i in range(0, N, batch_size):
 
-
+            # if we visited enough samples already
             if not n_visited_samples < n_coverage:
                 break
 
@@ -58,36 +63,24 @@ def run(X, z, f, batch_size, n_coverage, n_epochs, d_converged, rng):
             # calculate update candidate
             y = x[t-1] - lr * g + sqrt((2*lr)/xi) * w
 
-            # update
-            # if np.less_equal(y, kmax).all():
-            #     # logger.info(f"first if")
-            #     if np.greater_equal(y, kmin).all():
-            #         # logger.info(f"second if")
-            #         if dtw(x[t-1],y) < D:
-            #             # logger.info(f"third if")
-            #             x[t] = y
-            #         else:
-            #             # logger.info(f"failed third, distance: {dtw(x[t-1],y)} > {D}")
-            #             x[t] = x[t-1]
-            #     else:
-            #         # logger.info(f"failed second")
-            #         x[t] = x[t-1]
-            # else:
-            #     # logger.info(f"failed first")
-            #     x[t] = x[t-1]
+            # update step
             x[t] = y if np.less_equal(y, kmax).all() and np.greater_equal(y, kmin).all() and dtw(x[t-1],y) < D else x[t-1]
 
             n_visited_samples += batch_size
 
+    # make possible solutions a set
     x = np.unique(x, axis=0)
 
-    logger.info(f"Now calculating {x.shape[0]} frechet variations")
+    logger.info(f"Now calculating {x.shape[0]} Fréchet variations")
 
+    # calculate the Fréchet function for every possible solution
     f_array = np.array([frechet(val, X) for val in x])
+    
+    # find indices of lowest fréchet function (also indices of correlating x)
     best_x = np.argmin(f_array)
 
     # flatten x[best_x] if it is a single element list
     result = x[best_x].flatten() if m == 1 else x[best_x]
 
-    # return -np.sort(-f_array), result
+    # return x with lowest Fréchet function value and the array of all Fréchet function values
     return result, f_array
